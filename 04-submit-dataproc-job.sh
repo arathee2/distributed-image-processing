5f739932a1160beedd26f0b0dfa79fb7ef0c0103
Amandeep Rathee <arathee2@gmail.com>
2021-03-22 19:29:24 -0400

    Create 04-submit-dataproc-job.sh

---
+++ 04-submit-dataproc-job.sh
@@ -0,0 +1,9 @@
+curl https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml | gsutil cp - gs://${MYBUCKET}/haarcascade_frontalface_default.xml
+cd ~/cloud-dataproc/codelabs/opencv-haarcascade
+
+gcloud dataproc jobs submit spark \
+--cluster ${MYCLUSTER} \
+--jar target/scala-2.12/feature_detector-assembly-1.0.jar -- \
+gs://${MYBUCKET}/haarcascade_frontalface_default.xml \
+gs://${MYBUCKET}/imgs/ \
+gs://${MYBUCKET}/out/
